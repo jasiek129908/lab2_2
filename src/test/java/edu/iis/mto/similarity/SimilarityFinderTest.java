2f29a11095a02d2f74509cc5e117b25c0dc51518
Jan Oleksik <222529@edu.p.lodz.pl>
2021-03-22 18:30:22 +0100

    Dodanie testu testEmptySequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -2,13 +2,25 @@ package edu.iis.mto.similarity;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testEmptySequences() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                return SearchResult.builder().build();
+            }
+        });
+        int tab[] = new int[0];
+        int tab2[] = new int[0];
+        double result = similarityFinder.calculateJackardSimilarity(tab, tab2);
+        Assertions.assertEquals(1, result);
     }
 
 }
