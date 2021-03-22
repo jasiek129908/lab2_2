d6d1dbfd393ae7d75abab58ba2257652d57c63f4
Jan Oleksik <222529@edu.p.lodz.pl>
2021-03-22 18:55:25 +0100

    Dodanie testu sprawdzajacego poprawnosc dzialaina dla sekwncji o rownych dlugosciach

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -23,4 +23,25 @@ class SimilarityFinderTest {
         Assertions.assertEquals(1, result);
     }
 
+    @Test
+    void testCorrectResultWithSequencesOfEqualLength() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                SearchResult searchResult = null;
+                if (elem == 1) {
+                    searchResult = SearchResult.builder().withPosition(0).withFound(true)
+                            .build();
+                } else if (elem == 2) {
+                    searchResult = SearchResult.builder().withPosition(0).withFound(false)
+                            .build();
+                }
+                return searchResult;
+            }
+        });
+        int tab[] = {1, 2};
+        int tab2[] = {1, 3};
+        double result = similarityFinder.calculateJackardSimilarity(tab, tab2);
+        Assertions.assertEquals(1 / 3.0, result);
+    }
 }
